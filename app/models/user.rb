24e3accef92b5942f6b3b9d16a658ea09886dad0
Mar <rkx1901@gmail.com>
2014-09-04 21:34:03 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 	has_many :ratings
 end
