536b27faca2d426689825e2136d145178b644c03
& <pair+scyclow.tblumer3@devbootcamp.com>
2014-09-05 16:27:03 -0500

    Wrote api helper meths for kimono, namely kimono_parser

--- lib/tasks/categorize.rb
+++ lib/tasks/categorize.rb
@@ -1,4 +1,6 @@
 require 'rest_client'
+require 'pp'
+require 'pry'
 
 
 # returnVal = RestClient.get 'https://www.kimonolabs.com/api/8awe8hva?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'# json_str = returnVal.to_json
@@ -10,6 +12,25 @@ def add_categories(collection)
 	end
 end
 
-# returnVal = RestClient.get 'https://www.kimonolabs.com/api/9lezh1lc?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'
-# json_obj = JSON.parse(returnVal)
-# add_categories(json_obj)
+returnVal = RestClient.get 'https://www.kimonolabs.com/api/9lezh1lc?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'
+json_obj = JSON.parse(returnVal)
+fixed_json = add_categories(json_obj)
+
+def kimono_parser(json)
+  category = Category.new
+  json.each do |article_hash|
+    unless article_hash["category"] == category.name
+      category = Category.create(name:article_hash["category"])
+    end
+
+    title = article_hash["title"]["text"]
+    url = article_hash["title"]["href"]
+    source = article_hash["source"]
+    slug = article_hash["slug"]
+    Article.create(title: title,
+                   url: url,
+                   source: source,
+                   slug: slug,
+                   category_id: category.id)
+  end
+end
