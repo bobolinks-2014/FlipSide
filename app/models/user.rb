dcdaaaf19b769edd0aa25143e6a55cacb787f0aa
Mar <rkx1901@gmail.com>
2014-09-04 21:40:52 -0500

    added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 	has_many :ratings
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904202551) do
+ActiveRecord::Schema.define(version: 20140905023529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140904202551) do
   create_table "articles", force: true do |t|
     t.string   "title"
     t.string   "source"
+    t.string   "topic"
     t.string   "url"
     t.text     "slug"
     t.datetime "created_at"
@@ -43,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140904202551) do
 
   create_table "tags", force: true do |t|
     t.string   "name"
+    t.string   "sentiment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -54,5 +56,7 @@ ActiveRecord::Schema.define(version: 20140904202551) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
