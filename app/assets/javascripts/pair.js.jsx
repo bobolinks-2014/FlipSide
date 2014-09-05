3d0d7dcd0d67e7b32fa482bebf03c3dbf67658d0
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-05 17:12:24 -0500

    not working

--- app/assets/javascripts/pair.js.jsx
+++ app/assets/javascripts/pair.js.jsx
@@ -1,31 +1,44 @@
 /*** @jsx React.DOM */
-console.log("pair");
 // a pair model
 // this describes the pair functionality
 // this renders the pair
 // has two articles
-
 var pair = React.createClass({
 	getPairs: function(){
 		console.log("here")
 		request = $.get('pairs');
+		var pairs = [];
 		request.done(function(data){
-			article1 = data[0].article1
-			article2 = data[0].article2
-		});
+			$.each(data, function(index){
+				var articles = [];
+				articles.push(data[index].article1);
+				articles.push(data[index].article2);
+				pairs.push(this.renderArticles(articles));
+			}.bind(this));
+			this.setState({pairs: pairs});
+		}.bind(this));
 	},
-	renderArticles: function(){
-		
+	renderArticles: function(articles){
+		var article1 = articles[0];
+		var article2 = articles[1];
+		return(
+			<div>
+				<div className = 'article large-6 columns'>
+					<div>{article1.title}{article1.source}{article1.slug}</div>				
+				</div>
+				<div className = 'article large-6 columns'>
+					<div>{article1.title}{article2.source}{article2.slug}</div>
+				</div>
+			</div>
+		);
 	},
 	render:function(){
 		this.getPairs();
-		return (<div className='pair large-12 columns'> </div>);
+		return (<div className='pair large-12 columns'>hello</div>);
+		
 	}
 })
 
-// we got all the pairs
-// for each pair, render a pair
-// render its articles
 
 
 React.renderComponent(<pair/>,
@@ -33,11 +46,12 @@ React.renderComponent(<pair/>,
 
 // article model
 
-var article = React.createClass({
-	render:function(article){
-		return (
-			<div className = "article"> Article </div>
-			)
-	}
-})
+// var article = React.createClass({
+// 	render: function(article){
+// 		debugger;
+// 		return (
+// 			<div className = "article"> Article </div>
+// 			)
+// 	}
+// })
 
