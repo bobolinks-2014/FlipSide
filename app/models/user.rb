ecd9c84d95f49cff5820ddd966a115686beb5e0c
Mar <rkx1901@gmail.com>
2014-09-04 21:47:26 -0500

    added validations for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
 	has_many :ratings
+  has_secure_password
+	validates :password, length: { minimum: 6 }
 end
