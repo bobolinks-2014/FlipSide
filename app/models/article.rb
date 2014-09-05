92d5b73efa512990944054f91d5e43a45d1c5d65
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-05 13:31:07 -0500

    add lib

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,15 +4,6 @@ class Article < ActiveRecord::Base
 	has_many :tags
   belongs_to :category
 
-  # this needs to be called each time the request to kimono is made
-	def add_categories(collection)
-		category = ""
-		collection["results"]["collection1"].each do |article|
-			article["category"] == "" ? article["category"] = category : category = article["category"]
-		end
-	end
-	# returnVal = RestClient.get 'https://www.kimonolabs.com/api/9lezh1lc?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'
-	# json_obj = JSON.parse(returnVal)
-	# add_categories(json_obj)
+
 
 end
--- lib/tasks/categorize.rb
+++ lib/tasks/categorize.rb
@@ -3,7 +3,6 @@ require 'rest_client'
 
 # returnVal = RestClient.get 'https://www.kimonolabs.com/api/8awe8hva?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'# json_str = returnVal.to_json
 
-
 def add_categories(collection)
 	category = ""
 	collection["results"]["collection1"].each do |article|
