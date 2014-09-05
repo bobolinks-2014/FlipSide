6a0d32e1cfce9f00996c9ab36e8b815f7e62980e
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-05 12:06:38 -0500

    make kimono calls from ruby and add categories to all articles

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,20 @@
 require 'rest_client'
 
+
+# returnVal = RestClient.get 'https://www.kimonolabs.com/api/8awe8hva?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'# json_str = returnVal.to_json
+
+
+def add_categories(collection)
+	category = ""
+	collection["results"]["collection1"].each do |article|
+		unless article["category"] == ""
+			category = article["category"]
+			p article["category"]
+		else
+			article["category"] = category
+		end
+	end
+endg
 returnVal = RestClient.get 'https://www.kimonolabs.com/api/9lezh1lc?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'
-p JSON.parse(returnVal.to_json)
+json_obj = JSON.parse(returnVal)
+add_categories(json_obj)
