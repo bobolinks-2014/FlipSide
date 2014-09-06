92b22d731eac5cc8a3512820ebeaeb9b1d6bdaa9
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-06 11:50:54 -0500

    landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,15 @@
 
 $(function(){
   $(document).foundation();
-  console.log("how often do i print?");
-  renderPair();
+  $('body').append('<div id = "landing" class = "large-12 columns full-page"><div id="manifesto" class= "row"> <h1>We live in an <div class="accentWord"> information cocoon</div> of media that constantly mirrors our existing beliefs. So we built an <div class="accentWord">anti-echo</div> chamber. Welcome to the <div id ="enter" class="inline"><div id="flipWord" class = "inline">ᖷlip</div>/<div id="sideWord" class = "inline">Side</div></div></h1></div></div>')
+  $('#enter').on("click", function(e){
+    e.preventDefault();
+    $( '#landing' ).fadeOut( 1000 );
+    renderPair();
+  });
 
+  $('#enter').hover(function(){
+    $( this ).fadeOut( 1000 );
+    $( this ).fadeIn( 1000 );
+  });
 });
