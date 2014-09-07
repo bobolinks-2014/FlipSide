3f80c6390c20425205c35c49ed107a13f78ce5bb
Steven Pikelny & Tanner Blumer <pair+scyclow.tblumer3@devbootcamp.com>
2014-09-07 12:47:56 -0500

    added relevence to article_tags, and all creations thereof; added user_tags migration, added user queries to get tag data

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :article_tags
 	has_many :articles, :through => :article_tags
+  has_many :user_tags
+  has_many :users, through: :user_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
@@ -5,6 +6,9 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
 	has_many :ratings
+  has_many :user_tags
+  has_many :tags, through: :user_tags
+
   has_secure_password
 	validates :password, length: { minimum: 6 }
 
@@ -15,6 +19,41 @@ class User < ActiveRecord::Base
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
+
+  #makes the user_tag with an article and a true/false agreement value
+  def vote(article,agreement)
+    article.article_tags.each do |article_tag|
+      tag = article_tag.tag
+      relevance = article_tag.relevance
+      sentiment = article_tag.sentiment_score
+      UserTag.create!(tag: tag,
+                     user:self,
+                     relevance: relevance,
+                     sentiment_score: sentiment,
+                     agreement: agreement)
+    end
+  end
+ #return a hash with tags(associated to the user) and their weighted_tag_score
+  def opinions
+    opinions_hash = Hash.new(0)
+    self.tags.each do |tag|
+      opinions_hash[tag] = weighted_tag_score(tag)
+    end
+    opinions_hash
+  end
+  #make sentiment, agreement, relevance attr_reader
+  def weighted_tag_score(tag)
+    user_tags = self.user_tags.where(tag_id: tag.id)
+    numerator = 0
+    user_tags.each do |user_tag|
+      direction = user_tag.agreement ? 1 : -1
+      score = user_tag.sentiment_score*user_tag.relevance*direction
+      numerator += score
+    end
+    denominator = user_tags.pluck(:relevance).inject(:+)
+    weighted_tag_score = numerator/denominator
+  end
+
 
 	  private
 
