8805d5f9dc070050838721a30989a3ed5f62008c
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-07 14:31:46 -0500

    method get common tags in pair

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,6 +1,21 @@
+require 'pry'
 class Pair < ActiveRecord::Base
   belongs_to :category
   belongs_to :article1, :class_name => "Article"
   belongs_to :article2, :class_name => "Article"
 
+
+  def getCommonTags
+    commonTags = []
+    self.article1.tags.each do |tag1|
+      self.article2.tags.each do |tag2|
+        p tag1.name
+        p tag2.name
+        if tag1 == tag2
+          commonTags << [tag1, tag2]
+        end
+      end
+    end
+    commonTags
+  end
 end
