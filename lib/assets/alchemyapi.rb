c634cb14c90ade8d1c2ed176bf6d44b06edee615
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-07 22:33:05 -0500

    scrape national news. i messed up the last one

--- lib/assets/alchemyapi.rb
+++ lib/assets/alchemyapi.rb
@@ -98,7 +98,8 @@ class AlchemyAPI
 			# b3308acb4846ed61eef5260e4930d33ee5383ff5
 			# 68a8634ab9a7ee5ff59e15bdf35fa9be83009ec6
 			# 5d57c3197fbc790fe4e443d551672c5a8b2fc9d2
-			key = '5d57c3197fbc790fe4e443d551672c5a8b2fc9d2'
+			# b4445cdb812e14b45e9bf5c57bc30bb6fa344639
+			key = 'b4445cdb812e14b45e9bf5c57bc30bb6fa344639'
 			key.strip!
 
 			if key.empty?
