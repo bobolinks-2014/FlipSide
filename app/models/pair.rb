5bf9e35caa2f8602d3cc41819391b70abc8524d8
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-07 21:36:53 -0500

    remove my unused stuff

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -4,18 +4,4 @@ class Pair < ActiveRecord::Base
   belongs_to :article1, :class_name => "Article"
   belongs_to :article2, :class_name => "Article"
 
-
-  def getCommonTags
-    commonTags = []
-    self.article1.tags.each do |tag1|
-      self.article2.tags.each do |tag2|
-        p tag1.name
-        p tag2.name
-        if tag1 == tag2
-          commonTags << [tag1, tag2]
-        end
-      end
-    end
-    commonTags
-  end
 end
