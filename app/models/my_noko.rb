b3ea5452d40a80df3fae21b053ef1783fe156562
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-08 15:45:58 -0500

    nochanged

--- app/models/my_noko.rb
+++ app/models/my_noko.rb
@@ -65,7 +65,7 @@ class MyNoko
 
 
   def self.world_news_parse(url)
-    google_news_base_url = "https://news.google.com"
+    google_news_base_url = "httMps://news.google.com"
     doc = Nokogiri::HTML(open(url))
     url_array = []
     doc.css(".topic-list .persistentblue").each do |topic|
@@ -77,6 +77,6 @@ class MyNoko
 end
 
 doc = Nokogiri::HTML(open("https://news.google.com/news/section?pz=1&cf=all&ned=us&hl=en&q=Ukraine&topicsid=FRONTPAGE&ict=tnv0&topicnv=__AB21PFzgFYiVzdeR9RgYfVUQIcN3eLnuy0SFp6v4XPqFsz2R_VVVPTPpS7769omAMRTuPCvzJHGZs8Vo_QMvPwIM2l7HrErhSkFrVGE_85p2vIw6XonHyE3ia-6wbhMJSoyNWqPtZXWvcBW4IoIEZle-m2KYkxuGe-5qvDHKYKxSBXsKiUW2yQxCB8IccprleyCCNJX3gkyMk4QXxbxSGYIkfZkxpgEPeO2SL9F5IJW8-j0_EdG5iQ3UCAGVi9lBcs5-9bV2GlmR6AvjL_Od10HiJF5AfVc7sPaQ-Er29DKGyVWzN1znybnpOdidY6YY_mk1oE5qe4XX9K4T8vo0Fqk1Jp33FbxjQz468Q5IKuiX2ywuGnUo-NGf_QLj8NWiihBiaQytWrWl-giyLcagQKLHEpTgvOqHhx5Pmqn7Ipsa2Mx8wtn-Tkybk3mKujWZUc7IfZgJxqIF-oKL7EW7UetZMqd2dn-ADnyth2j09UzzeyBuKcgFpdCbZA4cFcF-90nQ-ZY8hizTcZmadXeWtaDVI4pvU6cH4bt4GcearTjyAxdTWB-y05CBuj9reBJb7e_yqyE7USwMcC8fUPUNry5FzyBjg4sGxzg1zFor9_saO2aTFLCVgAAGXxRsFys-ocPm2rsvtKpkkygocJUOFYw765Cb4bXgkHZdpw3eaJVEItWrM6oKxUmK1PTfEToWLHc7hlnt_FKRCrojMUoXOtF_Df6A9phyAw=="))
-p articles = doc.css('.esc-lead-article-title .titletext')[0].text()
+# p articles = doc.css('.esc-lead-article-title .titletext')[0].text()
 
 
