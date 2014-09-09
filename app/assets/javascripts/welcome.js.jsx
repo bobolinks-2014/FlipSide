d39629352ab7cfaffb49ef4e25f91bed666564b8
Stephanie Chou <stephanie.chou@outlook.com>
2014-09-09 12:49:57 -0500

    spin not working yet

--- app/assets/javascripts/welcome.js.jsx
+++ app/assets/javascripts/welcome.js.jsx
@@ -1,5 +1,26 @@
 /*** @jsx React.DOM */
+// SPIN //
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
 
+var target = document.getElementById('container');
+var spinner = new Spinner(opts).spin(target);
 
 // TAG MODEL //
 var TagCollection = React.createClass({
@@ -294,9 +315,9 @@ $("#signin_form").on('submit', function(e) {
 	request.done(function(response) {
 		if(response.success == true) {
 			$('#signin_button').foundation('reveal', 'close');
-			debugger;
 			$('.not_logged_in').hide();
 			$('.logged_in').show();
+			location.reload();
 			renderPair();
 		} else {
 		console.log('failed');
@@ -327,6 +348,7 @@ $("#signup_form").on('submit', function(e) {
 			$('#signup_button').foundation('reveal', 'close');
 			$('.not_logged_in').hide();
 			$('.logged_in').show();
+			location.reload();
 			renderPair();
 		} else {
 			$.each(response.error, function(i) {
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  def test
-		returnVal = RestClient.get 'https://www.kimonolabs.com/api/9lezh1lc?apikey=GsBpFLhrLVtXl8mIDnFj8zv8rRVrsKTn'
-		returnVal.to_json
-		if request.xhr?
-			render :json => returnVal
-		end
-  end
 end
